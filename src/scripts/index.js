91775cc5581fa1e9141fc9d6e94c072ceda4d8ce
Colin Rosati <colin.james.rosati@gmail.com>
2019-11-01 11:52:48 +0100

    adding responsive styles

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -14,16 +14,16 @@ const initApp = () => {
     document.getElementById("sort-btn").addEventListener("click", sortAsDes); // add button event listener
     document.getElementById("search-btn").addEventListener("click", sortResults, false); // search button event
 
-    window.onscroll = function() { stickyHead() };
+    window.onscroll = function() { _stickyHead() };
 
     // Get the header
     var header = document.getElementById("filter");
 
-    // Get the offset position of the navbar
+    // Get the offset position 
     var sticky = header.offsetTop;
 
     // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-    function stickyHead() {
+    function _stickyHead() {
         if (window.pageYOffset > sticky) {
             header.classList.add("sticky");
         } else {
